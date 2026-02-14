62ab0287bd0c743d745a34fb59c2b1573c46105f
Brian Hornbrook <brian_hornbrook@outlook.com>
2026-02-13 21:23:21 -0600

    added volume script

--- expense-tracker-base/expense-tracker-base/scripts.sh
+++ expense-tracker-base/expense-tracker-base/scripts.sh
@@ -1,3 +1,5 @@
 docker run -it --rm -v "${pwd}:/app" python sh -c "cd /app && pip install -r requirements.txt"
 ${pwd} # current directory
 sh -c # runs bash script
+
+docker run --mount type=volume,source=db-vol,target=/app -it postgres:17 bash
